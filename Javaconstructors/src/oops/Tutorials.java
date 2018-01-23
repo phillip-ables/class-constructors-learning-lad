89186dd11216f9afb68e0c0a117465d22093ab3b
Phillip <phillipables1@gmail.com>
2018-01-22 22:11:10 -0500

    call Student object with arg 66 (get name is null)

--- Javaconstructors/src/oops/Tutorials.java
+++ Javaconstructors/src/oops/Tutorials.java
@@ -7,7 +7,7 @@ package oops;
 public class Tutorials {
     public static void main(String[] args){
         
-        Student phillip = new Student("Phillip");
+        Student phillip = new Student(66);
         
         phillip.introduce();
         
