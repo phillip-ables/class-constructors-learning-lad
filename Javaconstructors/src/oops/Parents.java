94686e6a2a78a0c697e8f4ab1dfcdf8ed8180cc0
Phillip <phillipables1@gmail.com>
2018-01-22 22:42:19 -0500

    properties declared and Parents constructor with empy params and called in Tutorials class

--- Javaconstructors/src/oops/Parents.java
+++ Javaconstructors/src/oops/Parents.java
@@ -5,5 +5,11 @@ package oops;
  * @author user
  */
 public class Parents {
+    String mom;
+    int siblings;
     
+    public Parents(){
+        System.out.println("Constructor called with no parameters");
+        
+    }
 }
--- Javaconstructors/src/oops/Tutorials.java
+++ Javaconstructors/src/oops/Tutorials.java
@@ -10,5 +10,7 @@ public class Tutorials {
         Student phillip = new Student("Phillip",66);        
         phillip.introduce();
         
+        Parents mom = new Parents();
+        
     }
 }
